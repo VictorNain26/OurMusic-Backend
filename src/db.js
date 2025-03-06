9a2bedcb33e2a874057fed19b2636e37228215aa
Victor <victor.lenain26@gmail.com>
2025-03-06 16:08:08 +0100

    add db.js

---
+++ src/db.js
@@ -0,0 +1,9 @@
+// src/db.js
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+export default sequelize;
