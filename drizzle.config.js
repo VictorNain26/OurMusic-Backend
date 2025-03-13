48538b649b2ca5916a4f92e10043cc5493145aa4
Victor <victor.lenain26@gmail.com>
2025-03-13 10:51:38 +0100

    update dockerfile

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,8 @@ export default {
   out: './drizzle/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    connectionString:
+      process.env.DATABASE_URL ||
+      `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
   },
 };
