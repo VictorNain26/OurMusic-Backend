dae3b07777fb6c6d0dbbad8b42808c3faffdc3ac
Victor <victor.lenain26@gmail.com>
2025-03-20 15:32:35 +0100

    init better auth

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,6 @@ export default {
   schema: './src/db/schema.js',
   out: './drizzle',
   dialect: 'postgresql',
-  driver: 'pg',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL,
   },
