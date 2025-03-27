1ee24a5ba041a11a572cd226399e62a41f711f7e
Victor <victor.lenain26@gmail.com>
2025-03-27 17:30:33 +0100

    update auth.config.js with mailer service

--- src/config/env.js
+++ src/config/env.js
@@ -1,11 +1,11 @@
 export const env = {
   PORT: Bun.env.PORT || 3000,
   DATABASE_URL: Bun.env.DATABASE_URL,
-  ALLOWED_ORIGINS: ['https://ourmusic.fr', 'https://ourmusic-api.ovh'],
+  ALLOWED_ORIGINS: ['https://ourmusic.fr', 'https://ourmusic-api.ovh', 'http://localhost:5173'],
   BETTER_AUTH_SECRET: Bun.env.BETTER_AUTH_SECRET,
   GOOGLE_CLIENT_ID: Bun.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: Bun.env.GOOGLE_CLIENT_SECRET,
-  FRONTEND_BASE_URL: Bun.env.FRONTEND_BASE_URL,
+  FRONTEND_BASE_URL: ['https://ourmusic.fr', 'http://localhost:5173'],
 };
 
 // ✅ Vérification stricte des variables Better Auth
